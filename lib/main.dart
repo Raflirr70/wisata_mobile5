f9251087184e6dae97d77b8072010f9429071de8
Raflirr70 <raflirr70@gmail.com>
2024-11-08 15:10:51 +0700

    Praktikum Mobile ke 5 : Membuat Tampilan loading screen dan tampilan masuk dimana gambarnya dapat digeser

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:wisata_mobile5/mystatefulwidget.dart';
-import 'package:wisata_mobile5/mystatelesswidget.dart';
+import 'package:wisata_mobile5/materimodulscreens/mystatefulwidget.dart';
+// import 'package:wisata_mobile5/materimodulscreens/mystatelesswidget.dart';
+import 'package:wisata_mobile5/models/landing_model.dart';
+import 'package:wisata_mobile5/screens/landingpage.dart';
+import 'package:wisata_mobile5/screens/splashscreen.dart';
 void main(){
-//  runApp(Mystatelesswidget());
-  runApp(Mystatefulwidget());
+  // runApp(Mystatelesswidget());
+  // runApp(Mystatefulwidget());
+  runApp(TravelApp());
+  // runApp(LandingPage());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
